675e4674f075218d59b956e68d8527d977e0d480
Fun-Times91 <bayce91@gmail.com>
2026-02-11 16:41:44 +0100

    script connexion

---
+++ QuaiAntiqueFrontend/js/auth/signin.js
@@ -0,0 +1,43 @@
+document.getElementById("btnSignin").addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const email    = document.getElementById("EmailInput").value.trim();
+    const password = document.getElementById("PasswordInput").value;
+
+    const response = await fetch("http://localhost:8000/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        setToken(data.apiToken);
+        
+        const roles = data.roles || ['ROLE_USER'];
+        let highestRole = 'ROLE_USER';
+        
+        if (roles.includes('ROLE_ADMIN')) {
+            highestRole = 'ROLE_ADMIN';
+        } else if (roles.includes('ROLE_EMPLOYE')) {
+            highestRole = 'ROLE_EMPLOYE';
+        }
+        
+        setCookie("role", highestRole, 7, "/");
+        
+        localStorage.setItem('token', data.apiToken);
+        localStorage.setItem('roles', JSON.stringify(roles));
+        localStorage.setItem('email', data.user);
+        
+        window.location.href = "/QuaiAntique/QuaiAntiqueFrontend/";
+    } else {
+        alert("Erreur de connexion: " + (data.message ?? response.status));
+    }
+});
+
+
+
