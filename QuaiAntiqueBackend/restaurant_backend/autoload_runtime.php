386f970ce965875bfd9a8873c57a692afdd88eee
Fun-Times91 <bayce91@gmail.com>
2026-03-29 15:31:31 +0200

    fix railway runtime bootstrap and docker startup

---
+++ QuaiAntiqueBackend/restaurant_backend/autoload_runtime.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+if (true === (require_once __DIR__.'/vendor/autoload.php') || empty($_SERVER['SCRIPT_FILENAME'])) {
+    return;
+}
+
+$app = require $_SERVER['SCRIPT_FILENAME'];
+
+if (!is_object($app)) {
+    throw new TypeError(sprintf('Invalid return value: callable object expected, "%s" returned from "%s".', get_debug_type($app), $_SERVER['SCRIPT_FILENAME']));
+}
+
+$runtime = $_SERVER['APP_RUNTIME'] ?? $_ENV['APP_RUNTIME'] ?? 'Symfony\\Component\\Runtime\\SymfonyRuntime';
+$runtime = new $runtime(($_SERVER['APP_RUNTIME_OPTIONS'] ?? $_ENV['APP_RUNTIME_OPTIONS'] ?? []) + [
+    'project_dir' => __DIR__,
+]);
+
+[$app, $args] = $runtime
+    ->getResolver($app)
+    ->resolve();
+
+$app = $app(...$args);
+
+exit(
+    $runtime
+        ->getRunner($app)
+        ->run()
+);
